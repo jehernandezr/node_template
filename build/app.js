54d5e7e45aef23bbec42e00c53b194c8a3b0db20
Jonatan Hernandez <je.hernandezr@uniandes.edu.co>
2022-01-23 11:38:26 -0500

    template

---
+++ build/app.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+var port = 3000;
+app.get('/', function (req, res) {
+    res.send('Buenas este es un server xdggg');
+});
+app.listen(port, function () {
+    console.log("server is listening on ".concat(port));
+});
